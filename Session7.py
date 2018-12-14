0f03be87d5355fe31db3ec15b529fba7fc031d8f
MarsiusVictorius <40165456+MarsiusVictorius@users.noreply.github.com>
2018-12-14 08:00:53 +0000

    Add files via upload

---
+++ Session7.py
@@ -0,0 +1,76 @@
+game = [[0, 0, 0],
+        [0, 0, 0],
+	[0, 0, 0],]
+
+def win(current_game):
+	for row in game
+		print(row)
+		all_match = True
+		for item != row:
+		if item != row[0]:
+			all_match == False
+	if not not_match:
+		print("Winner")
+win(game)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""game = [[0, 0, 0],
+	    [0, 0, 0],
+		[0, 0, 0],]
+
+def game_board(game_map,player=0, row=0, column=0, just_display=False):
+	print("   0  1  2")
+	if not just_display:
+		game[row][column] = player
+	for count,row in enumerate(game_map):
+		print(count, row)
+	return game_map
+
+game_board(game,just_display=True)    
+game_board(game,player=1, row=2, column=1)
+"""
+
+
+
+
+
+
+
+
+
+"""
+game = [[0, 0, 0],
+	    [0, 0, 0],
+		[0, 0, 0],]
+
+def game_board(player=0, row=0, column=0, just_display=False):
+	print("   0  1  2")
+	if not just_display:
+		game[row][column] = player
+	for count,row in enumerate(game):
+		print(count, row)
+
+
+game_board(just_display=True)    
+#game[0][1] = 1 
+game_board(player=1, row=2, column=1)
+"""
+
